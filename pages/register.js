ffc70c77c0e8828cf0a8fa51cc93ad20dd31447f
Axel <axosar@gmail.com>
2021-07-27 10:49:17 -0500

    test

--- pages/register.js
+++ pages/register.js
@@ -1,8 +1,44 @@
+import { useState } from "react";
+
 const Register = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+
+  }
   return(
     <>
       <h1 className="jumbotron text-center bg-primary square">Register</h1>
-      <p>Register page</p>
+      <div className="container col-md-4 offset-md-4 pb-5">
+        <form onSubmit={handleSubmit}>
+          <input 
+            type="text" 
+            className="form-control mb-4 p-2" 
+            value={name} 
+            onChange={(e) => setName(e.target.value)}
+            placeholder="Enter name"
+            required
+          />
+          <input 
+            type="email" 
+            className="form-control mb-4 p-2" 
+            value={email} 
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Enter email"
+            required
+          />
+          <input 
+            type="password" 
+            className="form-control mb-4 p-2" 
+            value={password} 
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Enter password"
+            required
+          />
+        </form>
+      </div>
     </>
   )
 }
